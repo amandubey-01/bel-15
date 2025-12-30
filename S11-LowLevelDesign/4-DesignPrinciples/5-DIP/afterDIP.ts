6ae8eb9a085973b1313f3a625b2a903345b643ab
amandubey-01 <aman351dubey@gmail.com>
2025-12-30 17:49:26 +0530

    Dependency Inversion Principle

---
+++ S11-LowLevelDesign/4-DesignPrinciples/5-DIP/afterDIP.ts
@@ -0,0 +1,45 @@
+// Interface
+interface Engine {
+    start(): void;
+}
+
+// Low-level modules
+class PetrolEngine implements Engine{
+    start(): void {
+        console.log("Petrol Engine started");
+    }
+}
+
+
+class DieselEngine implements Engine{
+    start(): void {
+        console.log('Diesel Engine started')
+    }
+}
+
+// High-level modules
+class Car {
+    private engine: Engine;
+    // Dependency Injection
+    // The high-level module is dependent on some low-level module, so I am injecting the low-level module from
+    // outside.
+    constructor(engine : Engine){
+        this.engine = engine;
+    }
+
+    startCar(){
+        this.engine.start();
+        console.log('Car started')
+    }
+}
+
+
+let petrolEngine = new PetrolEngine();
+let merc: Car = new Car(petrolEngine);
+
+// We can simply create another Car running on Diesel Engine. This is achivable as we created an interface in betwen.
+
+// Dependency inversion principle.
+// DIP is achieved via Dependency Injection (DI)
+
+export{}
